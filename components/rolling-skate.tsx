a6384958e85c916bbf798f4e819eabec2df33aa0
MARCOS <marcossuarezr88@gmail.com>
2026-07-22 14:57:50 +0200

    style: aumenta el tamaño del patín animado (w-16/h-14 -> w-24/h-20)

--- components/rolling-skate.tsx
+++ components/rolling-skate.tsx
@@ -6,7 +6,7 @@ import skateLottie from "@/public/u9ctATSOoA.json";
 export function RollingSkate() {
   return (
     <>
-      <div className="animate-roll-skate-container absolute top-0 left-1/2 -translate-x-1/2 -translate-y-12 pointer-events-none w-16 h-14">
+      <div className="animate-roll-skate-container absolute top-0 left-1/2 -translate-x-1/2 -translate-y-16 pointer-events-none w-24 h-20">
         <Lottie
           animationData={skateLottie}
           loop={true}
